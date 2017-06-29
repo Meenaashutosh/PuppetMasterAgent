2435b3941aba79c3fe1db426aa900db0b9b104f7
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-29 12:26:47 +0530

    Create clone.pp

---
+++ clone.pp
@@ -0,0 +1,30 @@
+#Git_Clone configuration (Standalone)
+
+#git_install.pp
+'''
+include git
+git::config{'user.name':
+  value     => 'Meenaashutosh' 
+}
+
+git::config{'user.email':
+  value     => 'ashutoshmeena111@gmial.com'
+}
+'''
+
+vcsrepo { '/opt/puppet-docs': 
+  ensure   => present, 
+  provider => git,  
+  source   => 'https://github.com/Meenaashutosh/PuppetMasterAgent.git',
+}
+
+
+#output
+[root@demo-1 manifests]# puppet apply  git_install.pp
+Warning: Config file /etc/puppet/hiera.yaml not found, using Hiera defaults
+Notice: Compiled catalog for demo-1 in environment production in 0.15 seconds
+Notice: Finished catalog run in 2.02 seconds[root@demo-1 manifests]# puppet apply clone.pp
+Notice: Compiled catalog for demo-1 in environment production in 0.25 seconds
+Notice: Finished catalog run in 0.14 seconds
+'''
+
